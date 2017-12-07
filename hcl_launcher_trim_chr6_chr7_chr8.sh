5a395469969932cae05eabd161fd888bc40ccc53
HamLam <lamx0031@umn.edu>
2017-12-07 17:21:52 -0600

    Update hcl_launcher_trim_chr6_chr7_chr8.sh

--- hcl_launcher_trim_chr6_chr7_chr8.sh
+++ hcl_launcher_trim_chr6_chr7_chr8.sh
@@ -168,7 +168,7 @@ tr , '\n' < "$sample_path/ordered_genes_temp.txt" > "$sample_path/ordered_genes.
 #cp $template_pwd/chr6_chr7_chr8_ordered_genes.txt $sample_path/ordered_genes.txt
 echo "Searching chr6-8 genes"
 mv $sample_path/ordered_genes.txt $sample_path/ordered_genes.txt.orig
-perl $scripts_location/find_chr6_7_8.pl $template_pwd/TSOV2_Genes_With_Chr_listed $sample_path/ordered_genes.txt.orig
+perl $scripts_location/find_chr6_7_8.pl $template_pwd/TSOV2supp_genes_chr_listed $sample_path/ordered_genes.txt.orig
 cp $current_path/chr6_7_8_genes_file.txt $sample_path/ordered_genes.txt
 cp $current_path/chr6_7_8_genes_file.txt $sample_path/chr6_7_8_genes_file.txt
 echo "chr6-8 genes searched"
