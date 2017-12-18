257fe79f655cd143ab958ae6b9b5ecf54e4e8a74
HamLam <lamx0031@umn.edu>
2017-12-18 09:22:46 -0600

    Update Pre_Load_Control.sql

--- template_trim/Pre_Load_Control.sql
+++ template_trim/Pre_Load_Control.sql
@@ -1,4 +1,4 @@
-/* DROP TABLE IF EXISTS `cnv_control_name_bwa_pileup_no_dup`;*/
+DROP TABLE IF EXISTS `cnv_control_name_bwa_pileup_no_dup`;
 CREATE TABLE `cnv_control_name_bwa_pileup_no_dup`(
 chr VARCHAR(32),
 pos INT,
@@ -8,7 +8,7 @@ mapped_reads INT
 /* LOAD DATA LOCAL INFILE 'cnv_control_name_bwa_pileup_no_dup.txt' INTO TABLE `cnv_control_name_bwa_pileup_no_dup` FIELDS TERMINATED BY '\t';
 CREATE INDEX `cnv_control_name_pileup_nd` ON `cnv_control_name_bwa_pileup_no_dup`(chr,pos);*/
 
-/* DROP TABLE IF EXISTS `cnv_control_name_bwa_pileup`; */
+DROP TABLE IF EXISTS `cnv_control_name_bwa_pileup`;
 CREATE TABLE `cnv_control_name_bwa_pileup`(
 chr VARCHAR(32),
 pos INT,
@@ -17,7 +17,7 @@ coverage INT
 /* LOAD DATA LOCAL INFILE 'cnv_control_name_bwa_pileup.txt' INTO TABLE `cnv_control_name_bwa_pileup` FIELDS TERMINATED BY '\t';
 CREATE INDEX `cnv_control_name_bwa_pileup_i1` ON `cnv_control_name_bwa_pileup`(chr,pos);*/
 
-/* DROP TABLE IF EXISTS `cnv_control_name_bowtie_pileup`;*/
+DROP TABLE IF EXISTS `cnv_control_name_bowtie_pileup`;
 CREATE TABLE `cnv_control_name_bowtie_pileup`(
 chr VARCHAR(32),
 pos INT,
