302fe635d149e59df47caf9826db8808c070ea18
HamLam <lamx0031@umn.edu>
2018-01-04 17:56:01 -0600

    Update cnv_tables.sql

--- template_trim/cnv_tables.sql
+++ template_trim/cnv_tables.sql
@@ -5,18 +5,18 @@ SELECT DISTINCT A1.* FROM
 (SELECT DISTINCT * 
 FROM 
 `cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov`
-WHERE min_bowtie_bwa_ratio >  0.83 AND max_bowtie_bwa_ratio < 1.1 AND cnv_ratio > 0.3 AND cnv_ratio < 0.7 AND avg_window_coverage > 20 ) A
+WHERE min_bowtie_bwa_ratio >  0.83 AND max_bowtie_bwa_ratio < 1.1 AND cnv_ratio > 0.3 AND cnv_ratio < 0.74 AND avg_window_coverage > 20 ) A
 JOIN
 (SELECT DISTINCT gene_symbol, gene_num_exons, exon_contig_id, window_id 
 FROM 
 `cnv_sample_name_over_control_name_60bp_exon_ref2_med_gene_cov`
-WHERE min_bowtie_bwa_ratio >  0.83 AND max_bowtie_bwa_ratio < 1.1 AND cnv_ratio > 0.3 AND cnv_ratio < 0.7 AND avg_window_coverage > 20) B
+WHERE min_bowtie_bwa_ratio >  0.83 AND max_bowtie_bwa_ratio < 1.1 AND cnv_ratio > 0.3 AND cnv_ratio < 0.74 AND avg_window_coverage > 20) B
 USING(window_id)) A1
 JOIN
 (SELECT DISTINCT gene_symbol, gene_num_exons, exon_contig_id, window_id 
 FROM 
 `cnv_sample_name_over_control_name_60bp_exon_ref3_med_gene_cov`
-WHERE min_bowtie_bwa_ratio >  0.83 AND max_bowtie_bwa_ratio < 1.1 AND cnv_ratio > 0.3 AND cnv_ratio < 0.7 AND avg_window_coverage > 20) B1
+WHERE min_bowtie_bwa_ratio >  0.83 AND max_bowtie_bwa_ratio < 1.1 AND cnv_ratio > 0.3 AND cnv_ratio < 0.74 AND avg_window_coverage > 20) B1
 USING(window_id);
 
 DROP TABLE IF EXISTS `cnv_sample_name_over_control_name_joint_control`;
