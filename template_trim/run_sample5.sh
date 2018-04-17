8553bf2f08ecf6f27554db8434c6efd0a344413c
HamLam <lamx0031@umn.edu>
2018-04-17 13:42:40 -0500

    Update run_sample5.sh

--- template_trim/run_sample5.sh
+++ template_trim/run_sample5.sh
@@ -28,6 +28,14 @@ cd $working_dir
 
 _now=$(date +"%Y-%m-%d_%H-%M")
 
+ls -1 $working_dir/reference_genes_file > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+    echo "$working_dir/reference_genes_file exists"
+else
+echo "chr6:152654942-152655608 SYNE1" >> ${working_dir}/reference_genes_file
+echo "chr9:132584659-132585325 TOR1A" >> ${working_dir}/reference_genes_file
+echo "chr17:7573726-7574233 TP53" >> ${working_dir}/reference_genes_file
+fi
 
 ls -1 $working_dir/completed.txt > /dev/null 2>&1
 if [ "$?" = "0" ]; then
@@ -282,18 +290,18 @@ echo -n "Finished load_sample " >> $working_dir/time_check
 timecheck=`(date +"%Y-%m-%d [ %H:%M:%S ]")`;
 echo ${timecheck} >> $working_dir/time_check
 
-grep "create_reference.sql" $working_dir/completed.txt > /dev/null 2>&1
+grep "create_reference_g5.sql" $working_dir/completed.txt > /dev/null 2>&1
 if [ "$?" = "0" ]; then
-    echo "create_reference.sql already run"
+    echo "create_reference_g5.sql already run"
 else
-    echo "Run create_reference.sql"
-    mysql --socket=$BASE/thesock -u root cnv5 < create_reference.sql
+    echo "Run create_reference_g5.sql"
+    mysql --socket=$BASE/thesock -u root cnv5 < create_reference_g5.sql
     if [[ $? -ne 0 ]] ; then
-	echo "Run create_reference.sql failed" >&2
+	echo "Run create_reference_g5.sql failed" >&2
 	## mysqladmin --socket=$BASE/thesock shutdown -u root
 	exit 1
     else
-	echo "create_reference.sql" >> $working_dir/completed.txt
+	echo "create_reference_g5.sql" >> $working_dir/completed.txt
     fi
 fi
 
@@ -484,6 +492,48 @@ fi
 echo -n "Finished create_sample_coverage.sql " >> $working_dir/time_check
 timecheck=`(date +"%Y-%m-%d [ %H:%M:%S ]")`;
 echo ${timecheck} >> $working_dir/time_check
+
+grep "Get_3_random_ref_genes" $working_dir/completed.txt > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+    echo "Get_3_random_ref_genes already run"
+else
+ echo "Run Get_3_random_ref_genes"
+  mysqldump -u root --socket=$BASE/thesock --tab=$working_dir cnv5 cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov
+  chromo=$(awk '{FS=" ";print $2}' cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov.txt | sort -u | awk -F ":" '{print $1}')
+  digit=$(awk '{FS=" ";print $2}' cnv_sample_name_over_control_name_60bp_exon_ref1_med_gene_cov.txt | sort -u | awk -F ":" '{print $2}' | cut -c1)
+  colon=":"
+  chromosome=$chromo$colon$digit
+  r1=$(grep ${chromosome} ${working_dir}/reference_genes_file)
+  echo $r1
+   ref1_gene=$(echo $r1 | cut -d" " -f2)
+   echo -n "ref1: " >> ${working_dir}/Three_Ref_Genes
+   echo "$ref1_gene" >> ${working_dir}/Three_Ref_Genes
+
+  mysqldump -u root --socket=$BASE/thesock --tab=$working_dir cnv5 cnv_sample_name_over_control_name_60bp_exon_ref2_med_gene_cov
+   chromo=$(awk '{FS=" ";print $2}' cnv_sample_name_over_control_name_60bp_exon_ref2_med_gene_cov.txt | sort -u | awk -F ":" '{print $1}')
+   digit=$(awk '{FS=" ";print $2}' cnv_sample_name_over_control_name_60bp_exon_ref2_med_gene_cov.txt | sort -u | awk -F ":" '{print $2}' | cut -c1)
+   colon=":"
+   chromosome=$chromo$colon$digit
+   r2=$(grep ${chromosome} ${working_dir}/reference_genes_file)
+   echo $r2
+   ref2_gene=$(echo $r2 | cut -d" " -f2)
+   echo -n "ref2: " >> ${working_dir}/Three_Ref_Genes
+   echo "$ref2_gene" >> ${working_dir}/Three_Ref_Genes
+
+  mysqldump -u root --socket=$BASE/thesock --tab=$working_dir cnv5 cnv_sample_name_over_control_name_60bp_exon_ref3_med_gene_cov
+  chromo=$(awk '{FS=" ";print $2}' cnv_sample_name_over_control_name_60bp_exon_ref3_med_gene_cov.txt | sort -u | awk -F ":" '{print $1}')
+  digit=$(awk '{FS=" ";print $2}' cnv_sample_name_over_control_name_60bp_exon_ref3_med_gene_cov.txt | sort -u | awk -F ":" '{print $2}' | cut -c1)
+  colon=":"
+  chromosome=$chromo$colon$digit
+  r3=$(grep ${chromosome} ${working_dir}/reference_genes_file)
+  echo $r3
+  ref3_gene=$(echo $r3 | cut -d" " -f2)
+  echo -n "ref3: " >> ${working_dir}/Three_Ref_Genes
+  echo "$ref3_gene" >> ${working_dir}/Three_Ref_Genes
+
+ echo "Get_3_random_ref_genes" >> $working_dir/completed.txt
+
+fi
 
 grep "create_control_coverage.sql" $working_dir/completed.txt > /dev/null 2>&1
 if [ "$?" = "0" ]; then
@@ -789,7 +839,9 @@ else
 	echo "get_ordered_genes.sql" >> $working_dir/completed.txt
 	sed -e s,NULL,,g < sample_name_cnv_calls_on_ordered_genes_$_now.txt > sample_name_cnv_calls_on_ordered_genes_$_now.txt.bak
 	mv sample_name_cnv_calls_on_ordered_genes_$_now.txt.bak sample_name_cnv_calls_on_ordered_genes_$_now.txt
-	#mv sample_name_cnv_calls_on_ordered_genes_$_now.txt.bak sample_name_cnv_calls_on_ordered_genes.txt
+	mv sample_name_cnv_calls_on_ordered_genes_$_now.txt sample_name_cnv_calls_on_ordered_genes_$_now.txt.tmp
+        cat sample_name_cnv_calls_on_ordered_genes_$_now.txt.tmp >> ${working_dir}/Three_Ref_Genes
+        mv ${working_dir}/Three_Ref_Genes sample_name_cnv_calls_on_ordered_genes_$_now.txt
     fi
 fi
 echo -n "Finished get_ordered_genes.sql " >> $working_dir/time_check
